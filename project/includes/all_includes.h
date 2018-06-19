7e46119f2a18a3e8ef6c6d03812835665f93bc93
Adrien PUSEL <adpusel@student.42.fr>
2018-06-19 15:54:33 +0200

    save

---
+++ project/includes/all_includes.h
@@ -0,0 +1,8 @@
+
+#ifndef LEM_IN_ALL_INCLUDES_H
+# define LEM_IN_ALL_INCLUDES_H
+# include "../../libft/ft_library_header.h"
+
+
+
+#endif //LEM_IN_ALL_INCLUDES_H
---
+++ project/parseur/getter.c
@@ -0,0 +1,4 @@
+//
+// Created by Adrien PUSEL on 6/19/18.
+//
+
