df3ff47cd6d59e5282e522b6e5cdc23f0ec7be96
get <adrien.pusel@gmail.com>
2018-08-11 20:54:33 +0200

    je get tout les chemins correctement

--- create_map/visu/find_path.js
+++ create_map/visu/find_path.js
@@ -20,7 +20,6 @@ let find_path = (start, end) => {
 	tmp.pop()
 	tab_split.push(tmp)
   })
-  nb_f_running = tab_split[0].length
 
   function Fourmi(name)
   {
@@ -31,36 +30,27 @@ let find_path = (start, end) => {
   Fourmi.prototype.add_path = function (path) {
 	this.path.push(parseInt(path))
 	// console.log(this.path[path.length -1], end)
-	if (this.path[path.length -1] === end)
-	{
-	  nb_f_running--
-	  console.log("ok")
-	}
   }
 
   tab_split[0].map((el) => {
 	fourmis.push(new Fourmi(el.split("-")[0]))
   })
 
-  // tab_split.map((el) => {
-  //
-  // let split = el.split("-")
-  // })
+  tab_split.map((tab_split_el) => {
+	tab_split_el.map((el) => {
 
-
-  tab_split[0].map((el) => {
-	let split = el.split("-")
-	fourmis.map((f) => {
-
-
-	  if (f.name === split[0])
-	  {
-		console.log(f.name, split)
-		f.add_path(split[1])
-	  }
+	  let split = el.split("-")
+	  fourmis.map((f) => {
+		if (f.name === split[0])
+		{
+		  f.add_path(split[1])
+		}
+	  })
 	})
   })
-  console.log(tab_split)
-  console.log(fourmis)
-  console.log(nb_f_running)
+	fourmis.map((el) => {
+	    console.log(el.path) 
+	})
+  // console.log(tab_split)
+  // console.log(fourmis)
 }
