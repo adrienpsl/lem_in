57c1bdb0af9d480f74e43e4d1e8f84f78c2d7cdd
get <adrien.pusel@gmail.com>
2018-08-13 09:54:06 +0200

    save test

--- rendu/test/a.sh
+++ rendu/test/a.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
-for file in `cd fourmis ; ls`
-    do
-        if [[ "$file" != "lem" && "$file" != "a.sh" ]]
-        then
-                echo "$file"
-                ./lem < "fourmis/$file"
-        fi
-done
+function set_sep()
+{
+    echo "/*------------------------------------*\ "
+    echo "     -- $1 --"
+    echo "\*------------------------------------*/ "
+}
+
+function run_test()
+{
+    name_test="$1"
+    set_sep "test $name_test"
+
+    for file in `cd "$name_test" ; ls`
+        do
+            if [[ "$file" != "lem" && "$file" != "a.sh" ]]
+            then
+                    echo "\033[1;31m----> $file \033[0m"
+                    ./lem < "$name_test/$file" | head -n 1
+            fi
+    done
+    echo
+}
+
+
+#run_test "1__bad_ants"
+#run_test "2__err_room"
+run_test "3__link"
